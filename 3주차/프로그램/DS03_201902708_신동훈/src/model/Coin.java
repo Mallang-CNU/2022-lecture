ca9901301312967a4d65cdd030104e33a83d2cc0
ShinDongHun1 <huipulci1@naver.com>
2022-03-24 12:43:31 +0900

    3주차 보고서 수정, 인코딩 변경

--- 3주차/프로그램/DS03_201902708_신동훈/src/model/Coin.java
+++ 3주차/프로그램/DS03_201902708_신동훈/src/model/Coin.java
@@ -9,42 +9,23 @@ public class Coin {
 
     private int _value;
 
-    /**
-     * DEFAULT_VALUE 값을 가진 Coin 객체를 생성한다.
-     */
     public Coin() {
         this._value = DEFAULT_VALUE;
     }
 
-    /**
-     * givenValue 값을 가진 Coin 객체를 생성한다.
-     */
     public Coin(int givenValue) {
         this._value = givenValue;
     }
 
-    /**
-     * Coin 의 값을 반환한다
-     * @return 코인의 value
-     */
     public int value() {
         return _value;
     }
 
-    /**
-     * Coin 의 값을 설정한다
-     * @param newValue 설정할 값
-     */
     public void setValue(int newValue) {
         this._value = newValue;
     }
 
 
-    /**
-     * 현재 코인의 금액이 otherCoin과 같은지 확인한다
-     * @param otherCoin 비교할 대상 Coin
-     * @return 같으면 true, 다르다면 false
-     */
     @Override
     public boolean equals(Object otherCoin) {
         if (otherCoin.getClass() != Coin.class){
